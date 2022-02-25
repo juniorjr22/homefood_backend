7c2f972ff7c3734bed0c31aefbf6307612062458
Mariosan Pereira <mariosancardoso@foursales.com.br>
2022-02-24 21:53:14 -0300

    Criação do projeto

---
+++ src/test/java/br/com/homefood/HomefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
