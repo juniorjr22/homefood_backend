40b422bca5e356b1379121bfa276096d83a90601
Edir Junior <edirgomescjunior@gmail.com>
2022-03-03 18:26:09 -0300

    Foi criada a entidade Receita_Ingrediente e seus atributos.

---
+++ src/main/java/br/com/homefood/entity/Receita_Ingrediente.java
@@ -0,0 +1,16 @@
+package br.com.homefood.entity;
+
+public class Receita_Ingrediente {
+	
+	private Integer idReceita;
+	
+	private Integer idIngrediente;
+	
+	private Integer quantidadeIngrediente;
+	
+	private String descricaoIngrediente;
+
+}
+
+
+
