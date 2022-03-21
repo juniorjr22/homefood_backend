0ede73561340054425ed65b40fcc78c6436f4174
Mariosan Pereira <ti.junior.22@gmail.com>
2022-03-20 21:12:24 -0300

    Comment removal

--- src/main/java/br/com/homefood/service/impl/RecipeServiceImpl.java
+++ src/main/java/br/com/homefood/service/impl/RecipeServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class RecipeServiceImpl /*implements RecipeService*/ {
+public class RecipeServiceImpl implements RecipeService {
 
     @Autowired
     private RecipeRepository repository;
